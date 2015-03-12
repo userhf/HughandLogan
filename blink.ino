4f5d5166905c760c5d0c0b4f22e04e82b08a2434
userhf <userhf@users.noreply.github.com>
2015-03-12 08:57:35 -0700

    Updated lengths

--- blink.ino
+++ blink.ino
@@ -5,155 +5,144 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  delay(5000);
-  digitalWrite(13, HIGH);   //short
+  delay(5000);//5-second delay
+  digitalWrite(13, HIGH);//short
+  delay(100);  
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//short
+  delay(100);  
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//short
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//short
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(300);//space
+  digitalWrite(13, HIGH);//short
+  delay(100);
+  digitalWrite(13, LOW);    
+  delay(100);               
+  digitalWrite(13, HIGH);//short
   delay(100);              
   digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //short
+  delay(100);               
+  digitalWrite(13, HIGH);//long
+  delay(300);              
+  digitalWrite(13, LOW);
+  delay(300);//space
+  digitalWrite(13, HIGH);//long
+  delay(300);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//long
+  delay(300);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//short
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(300);//space
+  digitalWrite(13, HIGH);//short
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//short
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//short
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//short
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(700); //big space
+  digitalWrite(13, HIGH);//short
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//long
+  delay(300);
+  digitalWrite(13, LOW);
+  delay(300);//space
+  digitalWrite(13, HIGH);//long
+  delay(300);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//short
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//long
+  delay(300);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//short
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//short
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(700); //big space
+  digitalWrite(13, HIGH);//short
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//long
+  delay(300);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//short
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//short
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(300); //space
+  digitalWrite(13, HIGH);//long
+  delay(300);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//long
+  delay(300);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//long
+  delay(300);
+  digitalWrite(13, LOW);
+  delay(300); //space
+  digitalWrite(13, HIGH);//long
+  delay(300);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//long
+  delay(300);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);//short
   delay(100);              
   digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //short
+  delay(300); //space
+  digitalWrite(13, HIGH);//short
   delay(100);              
   digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
+  delay(100);
+  digitalWrite(13, HIGH);//long
+  delay(300);
+  digitalWrite(13, LOW);
+  delay(300);  //space
+  digitalWrite(13, HIGH);//long
+  delay(300);
   digitalWrite(13, LOW);    
-  delay(1000);  
-  delay(2000);  //space
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
+  delay(100);
+  digitalWrite(13, HIGH);//short
+  delay(100);
   digitalWrite(13, LOW);    
-  delay(1000);               
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(1000);               
-  digitalWrite(13, HIGH);   //long
-  delay(1000);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  delay(2000);  //space
-  digitalWrite(13, HIGH);   //long
-  delay(1000);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //long
-  delay(1000);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  delay(2000);  //space
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(4000); //big space
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //long
-  delay(1000);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  delay(2000);  //space
-  digitalWrite(13, HIGH);   //long
-  delay(1000);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //long
-  delay(1000);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  delay(4000); //big space
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //long
-  delay(1000);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  delay(2000); //space
-  digitalWrite(13, HIGH);   //long
-  delay(1000);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //long
-  delay(1000);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //long
-  delay(1000);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  delay(2000); //space
-  digitalWrite(13, HIGH);   //long
-  delay(1000);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //long
-  delay(1000);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  delay(2000); //space
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //long
-  delay(1000);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  delay(2000);  //space
-  digitalWrite(13, HIGH);   //long
-  delay(1000);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  digitalWrite(13, HIGH);   //short
-  delay(100);              
-  digitalWrite(13, LOW);    
-  delay(1000);
-  delay(4000);//end of message
 }
